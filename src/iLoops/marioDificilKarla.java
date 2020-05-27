12d1fe156a94b6631e7124911c34264cf921e3cb
Karla Lopez del Castillo <karla.lopez2@astrazeneca.com>
2020-05-27 18:24:11 -0500

    tarea may 27

--- src/iLoops/marioDificilKarla.java
+++ src/iLoops/marioDificilKarla.java
@@ -2,22 +2,26 @@ package iLoops;
 
 import libs.Input;
 
+import java.sql.SQLOutput;
+
 public class marioDificilKarla {
 
     public static void main(String[] args) {
+        int altura = 0;
         System.out.println("Proporcionar la altura");
-        int altura = Input.get_int();
+        altura = Input.get_int();
         int base= altura + 1;
         int caractGato = 1;
         int caractVacios = 0;
 
-        for (int pira = 0; pira < altura; pira++)
+        for (int pira = 0, n = altura; pira < n; pira++)
         {
             caractVacios = base - caractGato;
             crearLinea(caractGato,caractVacios);
             caractGato = caractGato + 1;
         }
     }
+
     private static void crearLinea(int caractGato, int caractVacios)
     {
         String linea = "";
@@ -29,7 +33,9 @@ public class marioDificilKarla {
         {
             linea = linea + "#";
         }
-        System.out.println(linea);
+
+        String reverse= new StringBuffer(linea).reverse().toString();
+        System.out.println(linea + " " +reverse);
     }
 
 }
--- src/jStrings/segReprodDif.java
+++ src/jStrings/segReprodDif.java
@@ -1,5 +1,7 @@
 package jStrings;
 
+import libs.Input;
+
 public class segReprodDif {
     public static void main(String[] args) {
         //Dada la duracion de una cancion en segundos, obtenga el numero de minutos y segundos en el formato del ejemplo
@@ -7,6 +9,12 @@ public class segReprodDif {
         //Ejemplo:
         //Duracion en segundos: 360
         //La cancion dura 6 minutos y 0 segundos
+        System.out.println("Favor de proporcionar la duracion de la cancion en segundos");
+        double duracionCancionSeg = Input.get_int();
+        double resultadoMinutos = duracionCancionSeg / 60;
+        System.out.println("la duracion de la cancion es: " + resultadoMinutos + " minutos ");
+
+
 
     }
 }
