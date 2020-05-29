d7dc2e642f1ed1cdb0ae98ed1b4fbe6596935c5e
Karla Lopez del Castillo <karla.lopez2@astrazeneca.com>
2020-05-29 14:46:58 -0500

    examen 29  mayo

---
+++ src/mercadoLibre/crearCuentaML.java
@@ -0,0 +1,21 @@
+package mercadoLibre;
+
+public class crearCuentaML {
+    public static void main(String[] args) {
+        String firstName = "Pepe";
+        String lastName= "Pecas";
+        String email = "test@tester.com";
+        String password = "Pa$$w0rd";
+
+        navegarURL();
+        validarHomePage();
+        crearCuenta();
+        validarCuentaCreada();
+
+    }
+
+    public void navegarURL(){}
+    public void validarHomePage(){}
+    public void crearCuenta(String firstName, String lastName, String email, String password){}
+    public void validarCuentaCreada(){}
+}
